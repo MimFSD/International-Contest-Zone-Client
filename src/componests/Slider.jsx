382e493fa26dfa475c5ad818db1da361f5837ca5
MimFSD <mimislam878@gmail.com>
2024-07-09 17:27:03 +0600

    Update Slider

--- src/componests/Slider.jsx
+++ src/componests/Slider.jsx
@@ -12,6 +12,7 @@ const Slider = () => {
         <div className="  bg-bottom" style={{ backgroundImage: 'url(https://i.ibb.co/xjyQqLf/how-works-bg.png)' }}>
 
             <h2 className="text-4xl text-center uppercase text-[#0ecdb9] py-4 font-bold">best contest creator</h2>
+            
             <p className="text-center text-white">Best Contest Creator is designed for those who want to create <br /> impactful contests quickly and efficiently.  With advanced features  and intuitive controls, <br />
 
              you can focus on making your contest memorable and enjoyable for all participants.</p>
